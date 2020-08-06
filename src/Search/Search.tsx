4fc6965b60a26eba4edd4b548d2b2104cea5fcec
andreas@andreas-hahn.at <andreas@andreas-hahn.at>
2020-08-06 17:06:53 +0200

    added api links

--- src/Search/Search.tsx
+++ src/Search/Search.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+// discover all movies https://api.themoviedb.org/3/discover/movie?api_key=
+// get poster image https://image.tmdb.org/t/p/w500/wO5QSWZPBT71gMLvrRex0bVc0V9.jpg
 export class Search extends React.Component {
 	constructor(props: any) {
 		super(props);
